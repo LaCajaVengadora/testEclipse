98074ebf3e468d54ebb48fc6f913557988667271
caja <schrodingercat06@gmail.com>
2023-12-10 15:02:03 -0300

    edited from master

--- src/testEclipse/Example.java
+++ src/testEclipse/Example.java
@@ -4,11 +4,12 @@ public class Example {
 
 	public static void main(String[] args) {
 		int n1 = 12; int n2 = 9;
-		System.out.println(addTo(n1, n2));
-		System.out.println(susTo(n1, n2));
+		System.out.println(addTo(n1, n2)); System.out.println(susTo(n1, n2));
+		System.out.println("Pepe");
 	}
 	
 	public static int addTo(int a ,int b) {return a+b;}
 	public static int susTo(int a ,int b) {return a-b;}
+	
 
 }
