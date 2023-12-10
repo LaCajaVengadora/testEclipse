9369d7deb98d87006685f51b7a94aab5b39a2105
caja <schrodingercat06@gmail.com>
2023-12-10 14:48:58 -0300

    start

---
+++ src/testEclipse/Example.java
@@ -0,0 +1,12 @@
+package testEclipse;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int n1 = 12; int n2 = 9;
+		System.out.println(addTo(n1, n2));
+	}
+	
+	public static int addTo(int a ,int b) {return a+b;}
+
+}
