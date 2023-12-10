ad02002b6476d0dcd887a979104cc1da96edbc16
caja <schrodingercat06@gmail.com>
2023-12-10 14:51:20 -0300

    update on Example

--- src/testEclipse/Example.java
+++ src/testEclipse/Example.java
@@ -5,8 +5,10 @@ public class Example {
 	public static void main(String[] args) {
 		int n1 = 12; int n2 = 9;
 		System.out.println(addTo(n1, n2));
+		System.out.println(susTo(n1, n2));
 	}
 	
 	public static int addTo(int a ,int b) {return a+b;}
+	public static int susTo(int a ,int b) {return a-b;}
 
 }
